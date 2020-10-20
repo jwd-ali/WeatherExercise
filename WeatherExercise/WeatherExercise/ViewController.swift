d05b3144f7db9a6f9d7d005de1a82c281c4a6204
jawad <jawad@digitify.com>
2020-10-20 12:35:48 +0500

    Initial commit

---
+++ WeatherExercise/WeatherExercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherExercise
+//
+//  Created by Jawad Ali on 20/10/2020.
+//  Copyright © 2020 Jawad Ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
